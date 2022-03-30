c877aed2af171cd6bc1a5bbc7949599163b3606f
etiennechu <etienne33@cyfoundry.com>
2022-03-30 04:36:29 +0000

    移除不可用功能V2.3.5

---
+++ Crontab/scripts/Suricata_updater.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+sleep $[ ( $RANDOM % 15 ) + 1 ]s
+ESM_API=`cat /.env | grep API_KEY_VALUE | cut -d = -f 2`
+current_rules_version=`curl -X POST "https://api.esm.secbuzzer.co/esmapi/web/file/fileVersion" -d "{'TypeCode': 'it'}" -H "Content-Type: application/json" -H "accept: */*" -H "authorization: $ESM_API" | cut -d : -f 2 | cut -d \" -f 2`
+local_rules_version=$(</tmp/local_rules_version)
+if [ "$current_rules_version" != "$local_rules_version" ]
+then
+    echo "[*] New version found! rules update to $current_rules_version"
+    curl -o /tmp/rules.tgz "https://api.esm.secbuzzer.co/esmapi/web/file/download/it/$current_rules_version" -H "accept: */*" -H "authorization: $ESM_API"
+    mkdir -p /tmp/rules
+    tar zxf /tmp/rules.tgz -C /tmp/rules
+    chown 1000:1000 /tmp/* -R
+    rsync -a --delete /tmp/rules /Suricata_rules/
+    rm -rf /tmp/rules /tmp/rules.tgz
+    echo "[*] Restarting Suricata"
+    sleep 3s
+    curl -XPOST --unix-socket /var/run/docker.sock -H 'Content-Type: application/json' http://localhost/containers/suricata/restart
+    rm /tmp/local_rules_version
+    echo $current_rules_version > /tmp/local_rules_version
+    echo "[*] Done!"
+fi
---
+++ Crontab/scripts/daily.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+
+# Auto remove eve-yyyy-mm-dd.json
+rm /suricata_log/eve-`date -d "-7 days" "+%Y-%m-%d"`.json 2>/dev/null
+echo "[*] remove eve-`date -d "-7 days" "+%Y-%m-%d"`.json"
+
+# Auto remove ES index
+eta_remove_date="$(date -d '-3 month' +%Y-%m)"
+# curl -X DELETE "elasticsearch:9200/eta-attack-$eta_remove_date"
+curl -X DELETE "elasticsearch:9200/eta-malware-$eta_remove_date"
+
+winlog_remove_date="$(date -d '-3 month' +%Y-%m)"
+curl -X DELETE "elasticsearch:9200/winlog-$winlog_remove_date"
+
+suricata_remove_date="$(date -d '-6 month' +%Y-%m)"
+curl -X DELETE "elasticsearch:9200/suricata-$suricata_remove_date"
+
+dtm_remove_date="$(date -d '-6 month' +%Y-%m)"
+curl -X DELETE "elasticsearch:9200/dtm-$dtm_remove_date"
+
+#cic_remove_date="$(date -d '-7 days' +%Y%m%d)"
+#curl -X DELETE "elasticsearch:9200/cic-$cic_remove_date"
+
+winlogbeat_remove_date="$(date -d '-7 days' +%Y%m%d)"
+curl -X DELETE "elasticsearch:9200/winlogbeat-$winlogbeat_remove_date"
+
+#packetbeat_remove_date="$(date -d '-7 days' +%Y.%m.%d)"
+#curl -X DELETE "elasticsearch:9200/packetbeat-$packetbeat_remove_date"
+
+#flowscan_remove_date="$(date -d '-6 month' +%Y-%m)"
+#curl -X DELETE "elasticsearch:9200/flowscan-$flowscan_remove_date"
+
+#nmapscan_remove_date="$(date -d '-6 month' +%Y-%m)"
+#curl -X DELETE "elasticsearch:9200/nmap-$nmapscan_remove_date"
+#echo "[*] Daily check and remove index"
+
+lm_remove_date="$(date -d '-184 day' +%Y-%m-%d)"
+curl -X DELETE "elasticsearch:9200/lm-$lm_remove_date"
+
+find /Logs/ -type f -ctime +3 | xargs tar zcvf `date +'%Y-%m-%d'`.tgz --remove-files
+find /Logs/ -type d -empty -delete
+mv `date +'%Y-%m-%d'`.tgz /LogsCompress
+
+chgrp 1000 /LogsCompress/*
+chown 1000 /LogsCompress/*
+
+#rm -f /LogsCompress/`date +'%Y-%m-%d' -d '-397 days'`.tgz
+
+echo [*] `date +'%Y-%m-%d'` exec RAW log compress >> /var/log/cron.log
+#echo [*] `date +'%Y-%m-%d'` Delete `date +'%Y-%m-%d' -d '-397 days'`.tgz  >> /var/log/cron.loga
